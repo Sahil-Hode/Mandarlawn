6d29e4370d06b1d987749e41669c036354024cbe
Sahil-Hode <sahilhode67@gmail.com>
2025-12-12 01:56:27 +0530

    last change

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.use("/api", mailRoutes);
 app.get('/', (req, res) => res.send('Hello World'));
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
 
