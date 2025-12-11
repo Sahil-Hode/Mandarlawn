fb8570e7fd581a51fdfb886f1c599c23de1ddff3
Sahil-Hode <sahilhode67@gmail.com>
2025-12-12 00:26:17 +0530

    initial commit change

--- server.js
+++ server.js
@@ -15,5 +15,6 @@ app.use("/api", mailRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Backend running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
+
